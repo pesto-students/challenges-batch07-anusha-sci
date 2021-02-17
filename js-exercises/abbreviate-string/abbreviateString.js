38bdb191f4f5ee1cacbcba6272a639d7e8e51213
Anusha <anusha19@navgurukul.org>
2021-02-17 13:59:37 +0530

    modify solution of abbreviate string

--- js-exercises/abbreviate-string/abbreviateString.js
+++ js-exercises/abbreviate-string/abbreviateString.js
@@ -2,15 +2,14 @@
 // isString is deprecated hence used typeof value === "string"
 
 function abbreviateString(str) {
-    if (typeof str ==='string') {
-        let str1 = str.trim();
-        let splitList = str1.split(' ');
-        if (splitList.length > 1) {
-            return splitList[0] + ' ' + splitList[splitList.length - 1][0].toUpperCase() + ".";
-        } 
-    } else {
-        return abbreviateString(str).toThrow();
-    }
-}
+  if (typeof str === 'string') {
+    const splitList = str.trim().split(' ');
+    if (splitList.length > 1) {
+      return `${splitList[0]} ${splitList[splitList.length - 1][0].toUpperCase()}.`;
+    } 
+    return splitList[0];
+  } 
+  return abbreviateString(str).toThrow();
+} 
 
 export { abbreviateString };
