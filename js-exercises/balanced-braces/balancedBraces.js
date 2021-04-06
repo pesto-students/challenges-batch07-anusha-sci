2a394323026afa86244012fc068101184ffcfdf1
Anusha <anusha19@navgurukul.org>
2021-04-06 14:03:54 +0530

    Modify balancedBraces solution

--- js-exercises/balanced-braces/balancedBraces.js
+++ js-exercises/balanced-braces/balancedBraces.js
@@ -1,24 +1,59 @@
-function balancedBraces(str) {
-  const bracesPair = {
-    ')': '(',
-    ']': '[',
-    '}': '{',
+// function balancedBraces(str) {
+//   const bracesPair = {
+//     ')': '(',
+//     ']': '[',
+//     '}': '{',
+//   };
+//   const bracesStack = [];
+//   for (const char of str) {
+//     if (Object.values(bracesPair).includes(char)) {
+//       bracesStack.push(char);
+//     } else if (char in bracesPair) {
+//       if (bracesPair.length !== 0 && bracesStack[bracesStack.length - 1] === bracesPair[char]) {
+//         bracesStack.pop();
+//       } else {
+//         return false;
+//       }
+//     }
+//   }
+//   return bracesStack.length === 0;
+// }
+
+// export {
+//   balancedBraces,
+// };
+
+function balancedBraces(expression) {
+  const bracesMap = {
+    '(': ')',
+    '{': '}',
+    '[': ']',
   };
-  const bracesStack = [];
-  for (const char of str) {
-    if (Object.values(bracesPair).includes(char)) {
-      bracesStack.push(char);
-    } else if (char in bracesPair) {
-      if (bracesPair.length !== 0 && bracesStack[bracesStack.length - 1] === bracesPair[char]) {
-        bracesStack.pop();
+
+  const openingBraces = Object.keys(bracesMap);
+  const closingBraces = Object.values(bracesMap);
+
+  const isOpeningBrace = (char) => openingBraces.includes(char);
+  const isClosingBrace = (char) => closingBraces.includes(char);
+  const stack = [];
+
+  for (const char of expression) {
+    if (isOpeningBrace(char) || isClosingBrace(char)) {
+      if (isOpeningBrace(char)) {
+        stack.push(char);
       } else {
-        return false;
+        const topItem = stack.pop();
+        if (bracesMap[topItem] !== char) {
+          return false;
+        }
       }
     }
   }
-  return bracesStack.length === 0;
+
+  if (stack.length) {
+    return false;
+  }
+  return true;
 }
 
-export {
-  balancedBraces,
-};
+export { balancedBraces };
